d24a512a6917d1c9609f4c906260716d77e4e972
Gilliam6 <wizzzardoz@icloud.com>
2022-02-19 00:34:49 +0300

    Daisyou ui for tailwindCSS installed

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/new_meetup":["static\u002Fchunks\u002Fpages\u002Fnew_meetup.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fnew_meetup"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
