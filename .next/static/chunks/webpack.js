a9fda24efd7a3a804c3be158066b2d7259a9f38e
Gilliam6 <wizzzardoz@icloud.com>
2022-02-20 14:29:13 +0300

    commit changes with TS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1895fbc1dcd0498"; }
+/******/ 		__webpack_require__.h = function() { return "c88c8c78775ef394"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
