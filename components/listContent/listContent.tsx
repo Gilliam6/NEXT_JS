5d3a41a04c8ad1091acdbc327529c791ccc02dc1
Gilliam6 <wizzzardoz@icloud.com>
2022-02-23 13:22:53 +0300

    Add component with table of items

---
+++ components/listContent/listContent.tsx
@@ -0,0 +1,35 @@
+function ContentList(){
+    return (
+        <div className="max-w-967px">
+            <table className="table-auto">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Amount</th>
+          <th>Price</th>
+          <th>Shop</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td>Malcolm Lockyer</td>
+          <td>1961</td>
+        </tr>
+        <tr>
+          <td>Witchy Woman</td>
+          <td>The Eagles</td>
+          <td>1972</td>
+        </tr>
+        <tr>
+          <td>Shining Star</td>
+          <td>Earth, Wind, and Fire</td>
+          <td>1975</td>
+        </tr>
+      </tbody>
+    </table>
+        </div>
+        );
+}
+
+export default ContentList;
