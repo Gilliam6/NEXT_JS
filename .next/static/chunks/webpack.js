416f009fe044a86b34df7eea7e8fa4efd2521ab1
Gilliam6 <wizzzardoz@icloud.com>
2022-02-23 13:11:58 +0300

    Navigation Bar component for groceries page added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a34ecbc979a453de"; }
+/******/ 		__webpack_require__.h = function() { return "ec70a8a01eed6f0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
