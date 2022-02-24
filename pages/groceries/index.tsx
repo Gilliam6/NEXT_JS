c891187a4be68a22fee76de59b8ec888b66bb69c
Maksym Ryndia <ryndya.maxim@gmail.com>
2022-02-24 05:02:46 +0200

    Fix some styles

--- pages/groceries/index.tsx
+++ pages/groceries/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const GroceryPage: NextPage = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-700">
       <h1>Grocery</h1>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 
 const LandPage: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-400 flex-nowrap">
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-700 flex-nowrap">
       <div>
         <h1 className="m-2 text-4xl font-black">Groceries Ninja</h1>
       </div>
